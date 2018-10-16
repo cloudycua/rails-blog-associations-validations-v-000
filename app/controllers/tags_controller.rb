348419df2b9824c38eb01684125488c0752b70bf
Christine Cua <cloudycua@gmail.com>
2018-10-16 02:06:01 +0000

    automatically backed up by learn

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -63,12 +63,12 @@ class TagsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
+    def set_tag
+      @tag = Post.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def post_params
-      params.require(:post).permit(:name)
+    def tag_params
+      params.require(:tag).permit(:name)
     end
 end
