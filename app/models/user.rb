2dd929e6134e572bcf783d04d44088a953b9d6a8
Christine Cua <cloudycua@gmail.com>
2018-10-16 01:56:16 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :post_tags
-  has_many :posts, through: :post_tags
+  has_many :posts
   validates :name, presence: true
   validates :name, uniqueness: true
 end
