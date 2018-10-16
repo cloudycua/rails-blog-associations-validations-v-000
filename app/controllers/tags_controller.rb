ecd5ef8f12c1cc0f3870736ae1abcb4ded0ffd99
Christine Cua <cloudycua@gmail.com>
2018-10-16 02:04:56 +0000

    automatically backed up by learn

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -24,15 +24,15 @@ class TagsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
+    @tag = Tag.new(tag_params)
 
     respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @post }
+      if @tag.save
+        format.html { redirect_to @tag, notice: 'Tag was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @tag }
       else
         format.html { render action: 'new' }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.json { render json: @tag.errors, status: :unprocessable_entity }
       end
     end
   end
