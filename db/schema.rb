ca6952226dcbf134237fb0de0f6f04e1f0395a5d
Christine Cua <cloudycua@gmail.com>
2018-10-16 01:43:44 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181016013801) do
+ActiveRecord::Schema.define(version: 20181016014045) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
@@ -21,11 +21,15 @@ ActiveRecord::Schema.define(version: 20181016013801) do
   end
 
   create_table "tags", force: :cascade do |t|
-    t.text "name"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.text "name"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
